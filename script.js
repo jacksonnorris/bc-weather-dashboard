1390b7e89666c348c1ca589e3864806c003929cd
jacksonnorris <jacksonnorr@gmail.com>
2022-03-03 13:05:05 -0500

    added functionality for getting current weather info based on a string parameter

---
+++ script.js
@@ -0,0 +1,29 @@
+const apiKey = 'c129e79ec49813641b21ef000a914d76';
+var currentWeatherNode;
+var city = 'Charlotte';
+var queryURL = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&type=accurate&APPID=${apiKey}`;
+
+const request = async url => {
+    const response = await fetch(url);
+    return response.ok ? response.json() : Promise.reject({error: 500})
+};
+const getWeatherInfo = async (newCity) => {
+    try {
+        city = newCity;
+        queryURL = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&type=accurate&APPID=${apiKey}`;
+        $('#currentCity').html(`<h2>${city} ${moment().format('L')}</h2>`);
+        const response = await request(queryURL);
+        var results = JSON.stringify(response);
+        var temperature = JSON.parse(results).main.temp;
+        var wind = JSON.parse(results).wind.speed;
+        var humidity = JSON.parse(results).main.humidity;
+        var lon = JSON.parse(results).coord.lon;
+        var lat = JSON.parse(results).coord.lat;
+        const ocURL = await request(`http://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&type=accurate&APPID=${apiKey}`);
+        var uvIndex = ocURL.current.uvi;
+        $('#currentWeather').html(`<p>Temp: ${temperature}°F</p><p>Wind: ${wind} MPH</p><p>Humidity: ${humidity}%</p><p>UV Index: ${uvIndex}</p>`);
+    } catch (err) { console.log(err); }
+};
+// $('#currentCity').html('<h2>Charlotte ' + moment().format('L') + '</h2>');
+getWeatherInfo('Snowmass');
+// $('#currentWeather').html(currentWeatherNode);
