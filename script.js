6f6f762932464dc38be3585263efefbf5764cc03
Jackson Norris <jackson@Jacksons-MacBook-Pro.local>
2022-03-04 10:49:58 -0500

    changed http to https

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const apiKey = 'c129e79ec49813641b21ef000a914d76';
 var currentWeatherNode;
 var city = 'Charlotte';
-var queryURL = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&type=accurate&APPID=${apiKey}`;
+var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&type=accurate&APPID=${apiKey}`;
 var dayArray;
 
 
@@ -14,7 +14,7 @@ const getWeatherInfo = async (newCity) => {
     try {
         localStorage.setItem('myCity', newCity);
         city = newCity;
-        queryURL = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&type=accurate&APPID=${apiKey}`;
+        queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&type=accurate&APPID=${apiKey}`;
         const response = await request(queryURL);
         var results = JSON.stringify(response);
         var localTime = new Date(JSON.parse(results).dt * 1000).toLocaleDateString("en");
@@ -24,12 +24,12 @@ const getWeatherInfo = async (newCity) => {
         var humidity = JSON.parse(results).main.humidity;
         var lon = JSON.parse(results).coord.lon;
         var lat = JSON.parse(results).coord.lat;
-        const ocURL = await request(`http://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&type=accurate&APPID=${apiKey}`);
+        const ocURL = await request(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&type=accurate&APPID=${apiKey}`);
         var uvIndex = ocURL.current.uvi;
         getDaily(ocURL);
         $('#currentWeather').html(`<p>Temp: ${temperature}°F</p><p>Wind: ${wind} MPH</p><p>Humidity: ${humidity}%</p><p>UV Index: ${uvIndex}</p>`);
     } catch (err) { 
-        console.log('err', JSON.stringify(err));
+        // .log('err', JSON.stringify(err));
         $('#currentCity').html(`<h2>${city} not found</h2><p>Please try searching again</p>`);
         $('#fiveDayForecast').html('');
         $('#currentWeather').html('');
